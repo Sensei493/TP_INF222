91834626373f8bff678fb1983785f64a8761b02b
Marcel520-pro <jerryfeudjio@gmail.com>
2026-03-20 23:56:25 +0100

    ajout des routes

--- src/app.js
+++ src/app.js
@@ -2,6 +2,8 @@ const express = require('express');
 require('./config/db');
 
 const app = express();
+const articleRoutes = require('./routes/article.routes');
+app.use('/api', articleRoutes);
 
 app.use(express.json());
 
